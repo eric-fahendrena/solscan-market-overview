d73e6973cf39958bfb2019954916da3ddc06a1de
Eric Fahendrena <ericfahendrena29@gmail.com>
2024-09-06 12:58:53 +0300

    updated code

--- index.js
+++ index.js
@@ -9,37 +9,27 @@ app.use(cors());
 
 (async () => {
 	const browser = await puppeteer.launch({
-		// args: ['--no-sandbox', '--disable-setuid-sandbox'],
 		headless: true,
-		// executablePath: process.env.CHROME_BIN || null
 	});
-
+	
 	/**
 	 * Market Overview
-	 */
+	*/
 	
-	const page = await browser.newPage();
-	console.log('Market Overview Page created successfully!');
-
 	const baseURL = 'https://solscan.io/token/63LfDmNb3MQ8mw9MtZ2To9bEA2M71kZUUGq5tiJxcqj9';
-	
+	const page = await browser.newPage();
 	
 	console.time('request');
-	
-	console.log('Loading', baseURL, '...');
-	await page.goto(baseURL, { waitUntil: 'networkidle2' });
-	console.log(baseURL, 'successfully loaded!');
+	console.log('Obtaining Market Overview data...');
 
-	await page.waitForSelector('#__next');
+	await page.goto(baseURL, { timeout: 120000, waitUntil: 'networkidle2' });
+	await page.waitForSelector('#__next', { timeout: 120000 });
 	
 	const element = await page.$('#__next div.flex.flex-col.gap-0.items-stretch.justify-start.h-full.min-h-screen div.rounded-2xl.shadow-m.bg-neutral0.p-4.lg.h-full');
 	
 	const text = await page.evaluate(el => el.innerText, element);
-	// console.log(elements1);
 	
 	const textArr = text.split('\n');
-	// const textArr1 = text1.split('\n');
-	// console.log(text1);
 	
 	const jsonStr = `{
 		"${textArr[0]}": {
@@ -52,30 +42,29 @@ app.use(cors());
 	}`;
 	
 	const parsedJson = JSON.parse(jsonStr);
+	console.log('Market Overview data has been obtained successfully!');
 
 	/**
 	 * Holders page
-	*/
-	console.log('Creating holders page ...')
-
+	 */
+	
 	const holdersPage = await browser.newPage();
 	const totalHoldersPagesNb = 100;
 	const minPerc = 0.1;
-	console.log('Holders page created successfully!');
-
+	
 	let shouldStop = false;
 	let qualifiedPerc = [];
+	
 	console.log('Looking for qualified percentage ...');
 	for (let i = 0; i < totalHoldersPagesNb; i++) {
-		await holdersPage.goto(`${baseURL}?page=${i+1}#holders`, { waitUntil: 'networkidle2' });
-		await holdersPage.waitForSelector('#__next');
+		await holdersPage.goto(`${baseURL}?page=${i+1}#holders`, { timeout: 120000, waitUntil: 'networkidle2' });
+		await holdersPage.waitForSelector('#__next', { timeout: 120000 });
 
 		const hTexts = await holdersPage.$$eval('#__next #account-tabs table tbody tr td', els => els.map(el => el.innerText));
-		// console.log(hTexts);
+
 		for (let j = 0; j < 10; j++) {
 			const holderArr = hTexts.slice((j * 6), (j * 6) + 6);
 			if (parseFloat(holderArr[4]) < minPerc) {
-				// console.log(holderArr)
 				shouldStop = true;
 				break
 			}
@@ -86,7 +75,6 @@ app.use(cors());
 		}
 	}
 
-	console.log(qualifiedPerc);
 	const qPercJsonStr = `
 		{
 			"ID": "${qualifiedPerc[0]}",
@@ -96,6 +84,8 @@ app.use(cors());
 		}
 	`;
 	const qPercJson = JSON.parse(qPercJsonStr);
+
+	console.log('Holders last qualified percentage has obtained successfully!');
 	
 	await browser.close();
 	
